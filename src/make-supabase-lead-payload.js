f12aa09b0cecf8c613f3ff6dd5b7ef784a757547
dariolazzano3-ops <dario.lazzano3@gmail.com>
2026-09-04 19:16:23 +0200

    PROJECT FERRARI: parameterize synthetic Make payload scope

--- src/make-supabase-lead-payload.js
+++ src/make-supabase-lead-payload.js
@@ -1,10 +1,19 @@
 import { buildMakeSupabaseLeadBridgePlan } from './make-supabase-lead-bridge.js';
 
-export function makeSupabaseSyntheticExecutionPayload() {
+const DEFAULT_SCOPE = Object.freeze({
+  customer_id: 'bakery-muller',
+  project_id: 'digital-system-v1',
+  project_uuid: '6b4b7f3a-8c6f-4d72-9be1-f2f8a3120101'
+});
+
+export function makeSupabaseSyntheticExecutionPayload(input = {}) {
+  const scope = {
+    customer_id: input.customer_id || DEFAULT_SCOPE.customer_id,
+    project_id: input.project_id || DEFAULT_SCOPE.project_id,
+    project_uuid: input.project_uuid || DEFAULT_SCOPE.project_uuid
+  };
   const plan = buildMakeSupabaseLeadBridgePlan({
-    customer_id: 'bakery-muller',
-    project_id: 'digital-system-v1',
-    project_uuid: '6b4b7f3a-8c6f-4d72-9be1-f2f8a3120101',
+    ...scope,
     staging_only: true,
     synthetic_test_data_only: true,
     real_customer_data: false,
